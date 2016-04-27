a6abb978f51155a3464ba4a51c1daae09e54996a
Takuya Katsurada <mail@nutcrack.io>
2016-04-27 21:36:33 +0900

    Refactored, parsing process

--- src/command.py
+++ src/command.py
@@ -349,99 +349,24 @@ class Command(object):
     def __call__(self, *, context=None):
         self.name.process(context=context, parameter=self.parameter)
 
+    Instructions = [
+        Push, Copy, Slide, Duplicate, Swap, Discard, Add, Sub, Mul, Div, Mod,
+        Store, Retrieve, Register, Call, Jump, Equal, Less, Return, End,
+        Cout, Iout, Cin, Iin
+    ]
+
     @classmethod
     def make(cls, code):
         cur, commands, count = 0, 0, len(code)
         while cur < count:
             token = code[cur:] if ((cur + 3) - count) >= 0 else code[cur:cur + 4]
-            if "".join(token[0:Push.Step]) == Push.Token:
-                cur += Push.Step
-                param, index = cls.parameter(code, cur)
-                cur += index
-                yield Command(Push, parameter={'param': param})
-            elif "".join(token[0:Copy.Step]) == Copy.Token:
-                cur += Copy.Step
-                param, index = cls.parameter(code, cur)
-                cur += index
-                yield Command(Copy, parameter={'param': param})
-            elif "".join(token[0:Slide.Step]) == Slide.Token:
-                cur += Slide.Step
-                param, index = cls.parameter(code, cur)
-                cur += index
-                yield Command(Slide, parameter={'param': param})
-            elif "".join(token[0:Duplicate.Step]) == Duplicate.Token:
-                cur += Duplicate.Step
-                yield Command(Duplicate)
-            elif "".join(token[0:Swap.Step]) == Swap.Token:
-                cur += Swap.Step
-                yield Command(Swap)
-            elif "".join(token[0:Discard.Step]) == Discard.Token:
-                cur += Discard.Step
-                yield Command(Discard)
-            elif "".join(token[0:Add.Step]) == Add.Token:
-                cur += Add.Step
-                yield Command(Add)
-            elif "".join(token[0:Sub.Step]) == Sub.Token:
-                cur += Sub.Step
-                yield Command(Sub)
-            elif "".join(token[0:Mul.Step]) == Mul.Token:
-                cur += Mul.Step
-                yield Command(Mul)
-            elif "".join(token[0:Div.Step]) == Div.Token:
-                cur += Div.Step
-                yield Command(Div)
-            elif "".join(token[0:Mod.Step]) == Mod.Token:
-                cur += Mod.Step
-                yield Command(Mod)
-            elif "".join(token[0:Store.Step]) == Store.Token:
-                cur += Store.Step
-                yield Command(Store)
-            elif "".join(token[0:Retrieve.Step]) == Retrieve.Token:
-                cur += Retrieve.Step
-                yield Command(Retrieve)
-            elif "".join(token[0:Register.Step]) == Register.Token:
-                cur += Register.Step
-                param, index = cls.parameter(code, cur)
-                cur += index
-                yield Command(Register, parameter={'param': param, 'location': commands})
-            elif "".join(token[0:Call.Step]) == Call.Token:
-                cur += Call.Step
-                param, index = cls.parameter(code, cur)
-                cur += index
-                yield Command(Call, parameter={'param': param, 'location': commands})
-            elif "".join(token[0:Jump.Step]) == Jump.Token:
-                cur += Jump.Step
-                param, index = cls.parameter(code, cur)
-                cur += index
-                yield Command(Jump, parameter={'param': param})
-            elif "".join(token[0:Equal.Step]) == Equal.Token:
-                cur += Equal.Step
-                param, index = cls.parameter(code, cur)
-                cur += index
-                yield Command(Equal, parameter={'param': param})
-            elif "".join(token[0:Less.Step]) == Less.Token:
-                cur += Less.Step
-                param, index = cls.parameter(code, cur)
-                cur += index
-                yield Command(Less, parameter={'param': param})
-            elif "".join(token[0:Return.Step]) == Return.Token:
-                cur += Return.Step
-                yield Command(Return)
-            elif "".join(token[0:End.Step]) == End.Token:
-                cur += End.Step
-                yield Command(End)
-            elif "".join(token[0:Cout.Step]) == Cout.Token:
-                cur += Cout.Step
-                yield Command(Cout)
-            elif "".join(token[0:Iout.Step]) == Iout.Token:
-                cur += Iout.Step
-                yield Command(Iout)
-            elif "".join(token[0:Cin.Step]) == Cin.Token:
-                cur += Cin.Step
-                yield Command(Cin)
-            elif "".join(token[0:Iin.Step]) == Iin.Token:
-                cur += Iin.Step
-                yield Command(Iin)
+
+            for instruction in cls.Instructions:
+                if "".join(token[0:instruction.Step]) == instruction.Token:
+                    cur += instruction.Step
+                    param, index = cls.parameter(code, cur) if instruction.IsNeedParameter else (None, 0)
+                    cur += index
+                    yield Command(instruction, parameter={'param': param, 'location': commands})
             commands += 1
 
     @classmethod
